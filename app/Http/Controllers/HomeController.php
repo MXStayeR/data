caacaa2b1febbf02d1c11e06c18f3959218e4ab3
kirill_ch <kir.chernenko@c8.net.ua>
2017-02-14 12:14:23 +0200

    Create connection to banner DB. Create DMP and Taxonomy models with relationships

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -4,7 +4,8 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Redis;
-
+use App\DMP;
+use App\Taxonomy;
 
 class HomeController extends Controller
 {
@@ -27,6 +28,17 @@ class HomeController extends Controller
     {
         
         return view('home');
+    }
+
+    public function test()
+    {
+        foreach(DMP::find(8)->taxonomies as $tax)
+        {
+            echo $tax->text."<br>";
+        }
+
+        //$tax = Taxonomy::where("id", "=", 22)->where("dmp_id", "=", 8)->first();
+        var_dump(Taxonomy::getDmpTaxonomy(8,20)->text); exit;
     }
     
 }
